ef3674cc03d5ea4c3080318c7dca06c0996f411e
Jaz <pjaziel723@gmail.com>
2023-02-01 19:54:02 -0600

    clases update

---
+++ cusrsoTSC/tipos_en_Ts/06-Clases.js
@@ -0,0 +1,14 @@
+var Person = /** @class */ (function () {
+    function Person(n, c, p) {
+        this.name = n;
+        this.isCool = c;
+        this.pets = p;
+    }
+    Person.prototype.sayHello = function () {
+        return "Hi, my name is ".concat(this.name, " and i have ").concat(this.pets, " pets");
+    };
+    return Person;
+}());
+var person1 = new Person('Danny', false, 1);
+var person2 = new Person('Sarah', true, 6);
+console.log(person1.sayHello());
---
+++ cusrsoTSC/tipos_en_Ts/06-Clases.ts
@@ -0,0 +1,20 @@
+class Person {
+	name:String;
+	isCool: boolean;
+	pets: number;
+
+	constructor(n:string, c:boolean, p:number){
+		this.name = n;
+		this.isCool = c;
+		this.pets = p;
+	}
+
+	sayHello(){
+		return `Hi, my name is ${this.name} and i have ${this.pets} pets`
+	}
+}
+
+const person1 = new Person('Danny', false, 1);
+const person2 = new Person('Sarah', true, 6);
+
+console.log(person1.sayHello());
