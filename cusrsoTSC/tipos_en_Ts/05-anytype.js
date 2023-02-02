249832c4a1a31cf2d93709ad6b7dc74388d7b538
Jaz <pjaziel723@gmail.com>
2023-02-01 19:24:48 -0600

    type Alias

--- cusrsoTSC/tipos_en_Ts/05-anytype.js
+++ cusrsoTSC/tipos_en_Ts/05-anytype.js
@@ -5,3 +5,18 @@ age = {
     years: 100,
     months: 2
 };
+var person1 = {
+    name: 'John',
+    id: 1
+};
+var person2 = {
+    name: 'Delia',
+    id: 2
+};
+var sayHello = function (person) {
+    return 'Hi ' + person.name;
+};
+var sayGoodbye = function (person) {
+    return 'Seeya ' + person.name;
+};
+console.log(sayHello(person1));
--- cusrsoTSC/tipos_en_Ts/05-anytype.ts
+++ cusrsoTSC/tipos_en_Ts/05-anytype.ts
@@ -7,3 +7,29 @@ age = {
 	years: 100,
 	months: 2
 }
+// type  Aliase sirve para evitar el DRY(Dont Repeat Yourself)
+type StringOrNumber = string | number;
+
+type PersonObject = {
+	name: string;
+	id: StringOrNumber;
+};
+
+const person1: PersonObject = {
+	name: 'John',
+	id: 	1,
+};
+
+const person2: PersonObject = {
+	name: 'Delia',
+	id: 	2,
+};
+const sayHello = (person: PersonObject) => {
+	 return 'Hi ' + person.name;
+};
+
+const sayGoodbye = (person: PersonObject) => {
+	 return 'Seeya ' + person.name;
+};
+
+console.log(sayHello(person1))
