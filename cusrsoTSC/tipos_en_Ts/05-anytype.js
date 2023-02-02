b768ad429e81b142b757dea405c7742ff129587c
Jaz <pjaziel723@gmail.com>
2023-02-01 19:14:10 -0600

    anytype ts

---
+++ cusrsoTSC/tipos_en_Ts/05-anytype.js
@@ -0,0 +1,7 @@
+//se recomieda evitar el tipo any para evitar bugs
+var age = '100';
+age = 100;
+age = {
+    years: 100,
+    months: 2
+};
---
+++ cusrsoTSC/tipos_en_Ts/05-anytype.ts
@@ -0,0 +1,9 @@
+
+//se recomieda evitar el tipo any para evitar bugs
+
+let age:any = '100'
+age = 100;
+age = {
+	years: 100,
+	months: 2
+}
